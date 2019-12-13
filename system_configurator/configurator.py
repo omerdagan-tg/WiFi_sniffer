b413371292565871b7b2e30cac6f4038599ebddc
גל צור <galzur01@gmail.com>
2019-12-13 15:52:19 +0200

    added more documentation

--- system_configurator/configurator.py
+++ system_configurator/configurator.py
@@ -8,12 +8,14 @@ def get_configurations():     # configurations from the user
     pass
 
 
-def update_ini(list):   # the list is a list of configurations
+def update_ini(list):   # the list is a list of configurations, in the future we'll have more lists.
     f = open(r"..\tcp_communications\client\configuration.ini", "w")    # path to ini file
-    f.write("start\n")
-    for i in list:
+    f.write("START\n")
+    f.write("PROTOCOL\n")
+    for i in list:      # write the protocols to sniff
         f.write(i + "\n")
-    f.write("end")
+
+    f.write("END")      # end of ini file
     f.close()
 
 
